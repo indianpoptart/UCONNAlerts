0024fbb4de8c069c6af5754486431c192a05c6e7
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2015-10-07 22:50:07 -0400

    v1.0 - GCM Push Service
    Tried to get GCM working
    It failed :(

--- UCONNAlerts/app/src/main/java/com/nikhilparanjape/uconnalerts/MainActivity.java
+++ UCONNAlerts/app/src/main/java/com/nikhilparanjape/uconnalerts/MainActivity.java
@@ -2,20 +2,52 @@ package com.nikhilparanjape.uconnalerts;
 
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
+
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+       // Log.d(TAG, "From: " + from);
+       // Log.d(TAG, "Message: " + message);
+
+        if (from.startsWith("/topics/")) {
+            // message received from some topic.
+        } else {
+            // normal downstream message.
+        }
+
+        // ...
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
