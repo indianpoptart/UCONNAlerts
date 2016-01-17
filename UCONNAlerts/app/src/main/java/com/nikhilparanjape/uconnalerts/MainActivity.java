fffc355849842f9ff3fad1ad4490c908be259f4e
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2016-01-17 18:31:15 -0500

    v0.1 -stuff
    idk, might start new

--- UCONNAlerts/app/src/main/java/com/nikhilparanjape/uconnalerts/MainActivity.java
+++ UCONNAlerts/app/src/main/java/com/nikhilparanjape/uconnalerts/MainActivity.java
@@ -2,18 +2,9 @@ package com.nikhilparanjape.uconnalerts;
 
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,12 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.google.android.gms.iid.InstanceID;
 
 import java.io.IOException;
 
